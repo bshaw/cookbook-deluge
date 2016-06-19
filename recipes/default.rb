a879c8c85539e9cfadc50e5d2edcdb6929ec9e0f
Brian Shaw <bshaw@virtusa.com>
2016-06-19 20:32:22 +0100

    Remove container host & domain names

--- recipes/default.rb
+++ recipes/default.rb
@@ -23,8 +23,6 @@ docker_container 'deluge' do
   tag 'latest'
   network_mode 'host'
   restart_policy 'always'
-  host_name node['deluge']['host_name']
-  domain_name node['deluge']['domain_name']
   volumes ['/etc/localtime:/etc/localtime:ro', "#{node['deluge']['config_volume']}:/config", "#{node['deluge']['downloads_volume']}:/downloads"]
 end
 
